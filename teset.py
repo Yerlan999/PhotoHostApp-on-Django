e53549f89ecf3740e01b51882ddc5c404d2e69e6
Yerlan Amir <fcbayernmunchenerlan999@gmail.com>
2020-10-08 20:40:02 +0600

    Slightly corrected Croppie Plugin with <div> tag

---
+++ teset.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+img = Image('test.jpg')
+
+print(dir(img))
+
+
--- users/static/users/js/crop.js
+++ users/static/users/js/crop.js
@@ -5,104 +5,118 @@ $(document).ready(function(){
 console.log("wooooo")
 
 
+var fff = $('a').eq(-1)[0].href
+console.log(fff)
+
+
+
+// THEN CROPPIE TAKES PLACE
+var user_profile_pic = $('#demo').croppie({
+
+  // viewport options
+  viewport: {
+    width: 250,
+    height: 250,
+    type: 'square' // or 'circle' square
+  },
+
+  update: function (data) {
+
+        var [left, upper, right, lower] = data['points'];
+        console.log(data['points'])
+        $('#id_left').val(left)
+        $('#id_right').val(right)
+        $('#id_lower').val(lower)
+        $('#id_upper').val(upper)
+  },
+  // boundary options
+  boundary: {
+    width: 500,
+    height: 500
+  },
+
+  // orientation controls
+  orientationControls: {
+    enabled: true,
+    leftClass: '',
+    rightClass: '',
+  },
+
+  // resize controls
+  resizeControls: {
+    width: true,
+    height: true
+  },
+
+  // addiontal CSS class
+  customClass: '',
+
+  // enable image zoom
+
+  // enable image resize
+  enableResize: true,
+
+  // show image zoom control
+  showZoomer: true,
+
+  // image zoom with mouse wheel
+  mouseWheelZoom: true,
+
+  // enable exif orientation reading
+  enableExif: true,
+
+  // restrict zoom so image cannot be smaller than viewport
+  enforceBoundary: true,
+
+  // enable orientation
+  enableOrientation: true,
+
+  // enable key movement
+  enableKeyMovement: true,
+
+
+});
+
+
+$('#demo').croppie('bind', {
+    url: fff,
+    orientation: 1,
+});
 
 
  // FOR GETTING IMAGE URL FROM INPUT TAG
 
 function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
 
-                reader.onload = function (e) {
+    if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-
-                    // SHOW UPLOADED(SELECTED) IMAGE
-
-                    $('#ProfilePicture').removeAttr('hidden');
-                    $('#ProfilePicture').attr('src', e.target.result);
-                    $('#cancelButton').removeAttr('hidden');
-                    $('#cropButton').removeAttr('hidden');
-
-                    // THEN CROPPIE TAKES PLACE
+    reader.onload = function (e) {
 
 
-                    var user_profile_pic = $('#ProfilePicture').croppie({
-                      // url: '',
+        // $('#ProfilePicture').attr('src', e.target.result);
+
+        $('#demo').croppie('bind', {
+            url: e.target.result,
+            orientation: 1,
+
+        });
+
+        // $('#cancelButton').removeAttr('hidden');
+        // $('#cropButton').removeAttr('hidden');
 
 
-                      // viewport options
-                      viewport: {
-                        width: 250,
-                        height: 250,
-                        type: 'square' // or 'circle' square
-                      },
-
-                      update: function (data) {
-
-                            var [left, upper, right, lower] = data['points'];
-
-                            $('#id_left').val(left)
-                            $('#id_right').val(right)
-                            $('#id_lower').val(lower)
-                            $('#id_upper').val(upper)
-                      },
-                      // boundary options
-                      boundary: {
-                        width: 500,
-                        height: 500
-                      },
-
-                      // orientation controls
-                      orientationControls: {
-                        enabled: true,
-                        leftClass: '',
-                        rightClass: ''
-                      },
-
-                      // resize controls
-                      resizeControls: {
-                        width: true,
-                        height: true
-                      },
-
-                      // addiontal CSS class
-                      customClass: '',
-
-                      // enable image zoom
-
-                      // enable image resize
-                      enableResize: false,
-
-                      // show image zoom control
-                      showZoomer: true,
-
-                      // image zoom with mouse wheel
-                      mouseWheelZoom: true,
-
-                      // enable exif orientation reading
-                      enableExif: true,
-
-                      // restrict zoom so image cannot be smaller than viewport
-                      enforceBoundary: true,
-
-                      // enable orientation
-                      enableOrientation: true,
-
-                      // enable key movement
-                      enableKeyMovement: true,
 
 
-                    });
 
-                    $('#cropButton').click(function(){
-                        user_profile_pic.croppie('result', 'base64').then(function(base64) {
-                        console.log(base64)
-                        $('#id_binary').val(base64)
-                        console.log("Showing Preview and Unhiding AJAX Button");
-                    });
+        // $('#cropButton').click(function(){
+        //     user_profile_pic.croppie('result', 'base64').then(function(base64) {
+
+        //     console.log("Showing Preview and Unhiding AJAX Button");
+        // });
 
 
-                    });
+        // });
 
                     // TEST SCOPE FOR AJAX HERE
 
@@ -112,10 +126,12 @@ function readURL(input) {
                 reader.readAsDataURL(input.files[0]);
             }
         }
-        // IF INPUT FIELD CHANGES
-        $("#id_image").change(function(){
-            readURL(this);
-        });
+
+
+// IF INPUT FIELD CHANGES
+$("#id_image").change(function(){
+    readURL(this);
+});
 
 
 
